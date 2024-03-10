a63317cbc299d7e541305b2cef70c0c50673a471
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-03-10 13:34:14 +0100

    adding alert message before deletion

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,13 +1,32 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import { CrossCircledIcon } from '@radix-ui/react-icons'
 import Link from 'next/link'
 
 const DeleteIssueButton = ({issueId}: {issueId:number}) => {
   return (
-    <Button color='red'>
-        <CrossCircledIcon/>
-        <Link href={`/issues/${issueId}/delete`}>Delete Issue</Link>  
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>
+          <CrossCircledIcon/>
+          delete
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content className='space-y-3'>
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? this action cannot be undone.
+        </AlertDialog.Description>
+        <Flex gap='4'>
+          <AlertDialog.Action>
+            <Button variant='surface' color='red'>Delete</Button>
+          </AlertDialog.Action>
+          <AlertDialog.Action >
+            <Button>Cancel</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
