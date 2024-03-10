7acb782a5a55b8d00560c585a96a9a9c52e55587
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-03-10 15:18:27 +0100

    add a drop down menu to show current user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { PiBugFill } from "react-icons/pi";
 import { usePathname } from 'next/navigation';
 import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Box } from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, Flex } from '@radix-ui/themes';
 
 const NavBar = () => {
     const currentPath = usePathname();
@@ -21,41 +21,57 @@ const NavBar = () => {
         }
     ]
   return (
-    <nav className="flex space-x-8 border-b mb-5 px-4 h-14 items-center">
-        <Link href="/">
-            <PiBugFill  size={25} />
-        </Link>
-        <ul className='flex space-x-8'>
-            {
-                links.map((link) => (
-                    <li key={link.href}>
-                        <Link className={classNames({
-                            'text-zinc-700' : currentPath === link.href,
-                            'text-zinc-500' : currentPath !== link.href,
-                            'hover:text-zinc-700 transition hover:text' : true
-                        })} href={link.href}>
-                            {link.label}
+    <nav className="border-b mb-5 px-5 py-3 h-14">
+        <Container>
+            <Flex justify='between'>
+                <Flex align='center' gap='3'>
+                    <Link href="/">
+                        <PiBugFill  size={25} />
+                    </Link>
+                    <ul className='flex space-x-8'>
+                        {
+                            links.map((link) => (
+                                <li key={link.href}>
+                                    <Link className={classNames({
+                                        'text-zinc-700' : currentPath === link.href,
+                                        'text-zinc-500' : currentPath !== link.href,
+                                        'hover:text-zinc-700 transition hover:text' : true
+                                    })} href={link.href}>
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </Flex>
+                <Box>
+                {
+                    status === 'authenticated' && session && ( 
+                        <DropdownMenu.Root>
+                            <DropdownMenu.Trigger>
+                                <Avatar size='2' src={session?.user!.image!} fallback="?" className='cursor-pointer'/>
+                            </DropdownMenu.Trigger>
+                            <DropdownMenu.Content>
+                                <DropdownMenu.Label>
+                                    {session?.user?.name}
+                                </DropdownMenu.Label>
+                                <DropdownMenu.Item>
+                                    <Link href='/api/auth/signout'>Sign out</Link>
+                                </DropdownMenu.Item>
+                            </DropdownMenu.Content>
+                        </DropdownMenu.Root>
+                    )
+                }
+                {
+                    status === 'unauthenticated' && (
+                        <Link href='/api/auth/signin'>
+                            Sign in
                         </Link>
-                    </li>
-                ))
-            }
-        </ul>
-        <Box>
-            {
-                status === 'authenticated' && session && ( 
-                    <Link href='/api/auth/signout'>
-                        Sign out
-                    </Link>
-                )
-            }
-            {
-                status === 'unauthenticated' && (
-                    <Link href='/api/auth/signin'>
-                        Sign in
-                    </Link>
-                )
-            }
-        </Box>
+                    )
+                }
+            </Box>
+            </Flex>
+        </Container>
     </nav>
   )
 }
