c4c2fa561fa221a52dcab376063594c4c9e3fb46
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-03-11 15:50:27 +0100

    Issue Summary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,38 @@
+import { Status } from '@prisma/client'
+import { Card, Flex, Text } from '@radix-ui/themes'
+import Link from 'next/link'
+import React from 'react'
+
+interface Props {
+    open: number
+    inProgress: number 
+    done: number
+}
+
+const IssueSummary = ({open, inProgress, done}: Props) => {
+    const containers: {
+        label:string
+        value:number
+        status:Status
+    } []= [
+        {label: 'Open Issues', value: open, status: 'OPEN'},
+        {label: 'In Progress Issues', value: inProgress, status: 'IN_PROGRESS'},
+        {label: 'Done Issues', value: done, status: 'DONE'}
+    ]
+  return (
+    <Flex gap='4'>
+        {
+            containers.map(({label, value, status}) => (
+                <Card key={status} >
+                    <Flex direction='column' gap='2'>
+                        <Link className='text-sm font-medium' href={`/issues?status==${status}`}>{label}</Link>
+                        <Text size='5' className='font-bold'>{value}</Text>
+                    </Flex>
+                </Card>
+            ))
+        }
+    </Flex>
+  )
+}
+
+export default IssueSummary
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
-import Pagination from "./components/Pagination";
 import WhatIsNextJS from "./components/WhatIsNextJS";
 
-export default function Home({searchParams} : {searchParams:{page: string}}) {
+export default async function Home({searchParams} : {searchParams:{page: string}}) {
+  const countOpenIssues = await prisma?.issue.count ({
+    where: {
+      status: 'OPEN'
+    }
+  })  || 0
+  const countDoneIssues = await prisma?.issue.count ({
+    where: {
+      status: 'DONE'
+    }
+  })  || 0
+  const countInProgressIssues = await prisma?.issue.count ({
+    where: {
+      status: 'IN_PROGRESS'
+    }
+  })  || 0
+
   return (
     <>
     <LatestIssues/>
+    <IssueSummary open={countOpenIssues} done={countDoneIssues} inProgress={countInProgressIssues} />
     <WhatIsNextJS />
     </>
   );
